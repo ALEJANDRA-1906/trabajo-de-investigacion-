3f1f2acbf19c1e63266fa08ec1405fb5146edd3d
vgarcia0906 <97006626+ALEJANDRA-1906@users.noreply.github.com>
2022-01-28 11:46:44 -0500

    Add files via upload

---
+++ cargog.py
@@ -0,0 +1,16 @@
+class Cargos2g:
+    secuencia_cargosp6 = 2
+    cargos = [{"codigo":1,"cargo":"Tester"},{"codigo":2,"cargo":"Director"}]
+    def __init__(self,cargo_g):
+        Cargos2g.secuencia_cargosg += 1
+        self.codigo_g = Cargos2g.secuencia_cargosg
+        self.cargo_g = cargo_g
+
+    def registro_cargosg(self):
+        return {"codigo":self.codigo_g,"cargo":self.cargo_g}
+
+    def mostrar_cargosg(self):
+        print("{} - {}".format(self.codigo_g,self.cargo_g))
+
+    def datos_cargosg(self):
+        return [self.codigo_g,self.cargo_g]
